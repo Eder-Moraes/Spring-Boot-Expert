1179162025bbe1a418a5f1779ae2387e2b924ded
Eder-Moraes <moraeseder880@gmail.com>
2024-12-13 10:46:45 -0300

    Utilizando @Qualifiers

--- ArquiteturaSpring/src/main/java/io/github/Eder_Moraes/ArquiteturaSpring/montadora/configuration/MontadoraConfiguration.java
+++ ArquiteturaSpring/src/main/java/io/github/Eder_Moraes/ArquiteturaSpring/montadora/configuration/MontadoraConfiguration.java
@@ -8,14 +8,36 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MontadoraConfiguration {
 
-    @Bean
-    public Motor motor(){
+    @Bean(name = "motorAspirado")
+    public Motor motorAspirado(){
         var motor = new Motor();
         motor.setCavalos(120);
         motor.setCilindro(4);
         motor.setLitragem(2.0);
         motor.setModelo("XPT-0");
         motor.setTipo(TipoMotor.ASPIRADO);
+        return motor;
+    }
+
+    @Bean(name = "motorEletrico")
+    public Motor motorEletrico(){
+        var motor = new Motor();
+        motor.setCavalos(110);
+        motor.setCilindro(3);
+        motor.setLitragem(1.4);
+        motor.setModelo("TH-10");
+        motor.setTipo(TipoMotor.ELETRICO);
+        return motor;
+    }
+
+    @Bean(name = "motorTurbo")
+    public Motor motorTurbo(){
+        var motor = new Motor();
+        motor.setCavalos(180);
+        motor.setCilindro(4);
+        motor.setLitragem(1.5);
+        motor.setModelo("TH-20");
+        motor.setTipo(TipoMotor.TURBO);
         return motor;
     }
 }
