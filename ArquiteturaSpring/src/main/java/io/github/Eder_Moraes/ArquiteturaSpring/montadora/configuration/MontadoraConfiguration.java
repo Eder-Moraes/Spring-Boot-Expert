3aa55fd6a83ada6dda6f3c66eb13b88019a2fede
Eder-Moraes <moraeseder880@gmail.com>
2024-12-13 10:58:32 -0300

    Beans Primary e annotations de Qualifiers

--- ArquiteturaSpring/src/main/java/io/github/Eder_Moraes/ArquiteturaSpring/montadora/configuration/MontadoraConfiguration.java
+++ ArquiteturaSpring/src/main/java/io/github/Eder_Moraes/ArquiteturaSpring/montadora/configuration/MontadoraConfiguration.java
@@ -4,6 +4,7 @@ import io.github.Eder_Moraes.ArquiteturaSpring.montadora.Motor;
 import io.github.Eder_Moraes.ArquiteturaSpring.montadora.TipoMotor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class MontadoraConfiguration {
@@ -20,6 +21,7 @@ public class MontadoraConfiguration {
     }
 
     @Bean(name = "motorEletrico")
+    @Primary
     public Motor motorEletrico(){
         var motor = new Motor();
         motor.setCavalos(110);
